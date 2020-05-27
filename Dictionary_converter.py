7d7198358359c0749143766fa912e098efb99c9c
Doyeon Lim <dyklim@gmail.com>
2020-05-27 14:28:18 +0900

    bug fix, new function: wrong count problem, make new word section, file initialization
    1. Create folder and file if it is not exist.
    more detail log is printed when some error occurs
    2. Counting words has some problem. fixed
    3. Made a new word sections.
    newly added words are written on 'newly added words' section.
    colored Blue.
    4. Emphasizing if newly added word has seen before.
    colored Red.

--- Dictionary_converter.py
+++ Dictionary_converter.py
@@ -5,9 +5,12 @@
 
 
 bookname = input('Put book/movie name: ')
-
+bookname = '_'.join(bookname.split())
 
 # In[2]:
+
+import pandas as pd
+import sys
 
 
 import os
@@ -17,41 +20,47 @@ from os import path
 from datetime import datetime
 
 if not os.path.exists(bookname):
-    print('[{}] Create folders and files that you need'.format(datetime.datetime.now().time()))
+    print('[{}] Create folders and files that you need'.format(datetime.now().time()))
     os.makedirs(bookname)
     pathprefix = './'+bookname+'/'
-    files = [pathprefix+bookname+'_new.xlsx', pathprefix+bookname+'_DB.csv']
-    for file in files:
-        f = open(file, 'w')
-        f.close()
-
+    files = [pathprefix+bookname+'_new.xslx', pathprefix+bookname+'_DB.csv']
+    nondf = pd.DataFrame()
+    nondf.to_csv('./'+bookname+'/'+bookname+'_DB.csv', index = False)
+    nondf.to_excel('./' + bookname + '/' + bookname + '_new.xlsx', index=False)
+    f = open('./' + bookname + '/' + bookname + '_DB.csv', 'w')
+    f.close()
 
 # In[3]:
 
 
-import pandas as pd
-
-
 try:
-    newdata = pd.read_excel('./'+bookname+'/'+bookname+'_new.xlsx',header = None, names = ['Eng','Kor'])
+    newdata = pd.read_excel('./'+bookname+'/'+bookname+'_new.xlsx',header = None, names = ['Eng','Kor'], sep=",\t")
     print('[{}] Reading {}_new.xlsx file complete'.format(datetime.now().time(), bookname))
 except:
-    print('{}_new.xlsx file doesn\'t exist'.format(bookname))
-    print('[{}] Creating {}_new.xlsx file...'.format(datetime.now().time(), bookname))
-    f = open('./'+bookname+'/'+bookname+'_new.xlsx', 'w')
-    newdata = pd.DataFrame()
-    f.close()
+    if not os.path.exists('./'+bookname+'/'+bookname+'_new.xlsx'):
+        print('{}_new.xlsx file doesn\'t exist or empty'.format(bookname))
+        print('[{}] Creating {}_new.xlsx file...'.format(datetime.now().time(), bookname))
+    #f=open('./' + bookname + '/' + bookname + '_new.xslx','w')
+    nondf = pd.DataFrame()
+    nondf.to_excel('./' + bookname + '/' + bookname + '_new.xlsx', index=False)
+    newdata = pd.DataFrame(columns = ['Eng','Kor'])
+
+if newdata.empty:
+    print('[{}] {}_new.xlsx file is empty!'.format(datetime.now().time(), bookname))
+    print('Please put words in {}_new.xlsx file!'.format(bookname))
+    sys.exit()
+
 
 try:
     DB=pd.read_csv('./'+bookname+'/'+bookname+'_DB.csv')
     print('[{}] Loading DB file complete'.format(datetime.now().time(), bookname))
 except:
-    print('{}_DB.csv file doesn\'t exist'.format(bookname))
-    print('[{}] Creating {}_DB.csv file...'.format(datetime.now().time(), bookname))
-    f = open('./'+bookname+'/'+bookname+'_DB.csv', 'w')
-    DB = pd.DataFrame()
+    if not os.path.exists('./' + bookname + '/' + bookname + '_DB.csv'):
+        print('{}_DB.csv file doesn\'t exist'.format(bookname))
+        print('[{}] Creating {}_DB.csv file...'.format(datetime.now().time(), bookname))
+    f = open('./'+bookname+'/'+bookname+'_DB.csv' , 'w')
+    DB = pd.DataFrame(columns = ['Eng','Kor'])
     f.close()
-
 
 # In[4]:
 
@@ -98,30 +107,9 @@ for eng, kor in engdict.items():
 data = pd.DataFrame({'Eng': englst,'Kor': korlst,'cnt': cntlst})
 
 
-# In[8]:
-
-
-print('[{}] Saving new DB...'.format(datetime.now().time()))
-data.to_csv('./'+bookname+'/'+bookname+'_DB.csv',index=False,encoding='utf-8-sig')
-
-
-# In[9]:
-
-
-nondf = pd.DataFrame()
-
-
-# In[10]:
-
-
-print('[{}] Clearing {}_new.xlsx file...'.format(datetime.now().time(), bookname))
-nondf.to_excel('./'+bookname+'/'+bookname+'_new.xlsx', index = False)
 
 
 # In[11]:
-
-
-data['startletter'] = data.Eng.str[0].str.lower()
 
 
 # In[13]:
@@ -139,42 +127,68 @@ document = Document()
 
 height = 0.3
 
-print('[{}] most frequent words part...'.format(datetime.now().time()))
+print('[{}] newly added words part...'.format(datetime.now().time()))
  
 document.add_heading(bookname+' Vocabulary', 0)
 data = data.sort_values('cnt', ascending=False)
-p = document.add_heading('Frequently confused words',level=1)
 
-table_fq = document.add_table(data.shape[0]+1, data.shape[1]-1)
+p = document.add_heading('Newly added words',level=1)
+
+table_new = document.add_table(newdata.shape[0]+1, newdata.shape[1])
+hdr_cells = table_new.rows[0].cells
+
+for j in range(newdata.shape[1]):
+    run = hdr_cells[j].paragraphs[0].add_run(newdata.columns[j])
+    run.bold = True
+    run.underline = True
+
+for i in tqdm.tqdm(range(newdata.shape[0])):
+    for j in range(newdata.shape[-1]):
+        cell = table_new.cell(i+1,j)
+        cell.text = str(newdata.values[i,j])
+        run = cell.paragraphs[0].runs[0]
+        run.font.color.rgb = RGBColor(0x00, 0x00, 0xFF)
+
+for row in table_new.rows:
+    row.height = Cm(height)
+
+print('[{}] most frequently confusing words part...'.format(datetime.now().time()))
+p = document.add_heading('Frequently confusing words',level=1)
+
+cntMoreThanOne = sum(data.cnt > 1)
+table_fq = document.add_table(cntMoreThanOne+1, data.shape[1])
 
 hdr_cells = table_fq.rows[0].cells
 
 # add the header rows.
-for j in range(data.shape[-1]-1):
+for j in range(data.shape[-1]):
     run = hdr_cells[j].paragraphs[0].add_run(data.columns[j])
     run.bold = True
     run.underline = True
     #table_fq.cell(0,j).text = data.columns[j]
 
 # add the rest of the data frame
-check = False
-newreg = False
+seenbefore = False
+#newreg = False
+idx = 0
 for i in tqdm.tqdm(range(data.shape[0])):
-    check = False
-    newreg = False
+    if data.values[i,2] < 2: continue
+    seenbefore = False
+    #newreg = False
     if str(data.values[i,0]) in seen:
-        check = True
-    if str(data.values[i,0] in newwords):
-        newreg = True
-    for j in range(data.shape[-1]-1):
-        cell = table_fq.cell(i+1,j)
+        seenbefore = True
+    #if str(data.values[i,0]) in newwords:
+    #    newreg = True
+    for j in range(data.shape[-1]):
+        cell = table_fq.cell(idx+1,j)
         cell.text = str(data.values[i,j])
-        if check:
+        if seenbefore:
             run = cell.paragraphs[0].runs[0]
             run.font.color.rgb = RGBColor(0xFF, 0x00, 0x00)
-        elif newreg:
-            run = cell.paragraphs[0].runs[0]
-            run.font.color.rgb = RGBColor(0x00, 0x00, 0xFF)
+        #elif newreg:
+        #    run = cell.paragraphs[0].runs[0]
+        #    run.font.color.rgb = RGBColor(0x00, 0x00, 0xFF)
+    idx += 1
         
 
 for row in table_fq.rows:
@@ -184,12 +198,13 @@ for row in table_fq.rows:
 data["lower"] = data["Eng"].str.lower()
 data.sort_values(['lower'], axis=0, ascending=True, inplace=True)
 del data['lower']
+save_data = data.copy()
 del data['cnt']
-
 
 # In[14]:
 
 
+data['startletter'] = data.Eng.str[0].str.lower()
 print('[{}] word dictionary part...'.format(datetime.now().time()))
 p = document.add_heading('Word Dictionary',level=1)
 data_group_letter = data.groupby('startletter')
@@ -199,17 +214,40 @@ for i in range(26):
         p = document.add_heading(ch.upper(),level=2)
         groupdata = data_group_letter.get_group(ch)
 
-        table_dict = document.add_table(groupdata.shape[0], groupdata.shape[1]-1)
+        table_dict = document.add_table(groupdata.shape[0], groupdata.shape[1]-2)
 
         # add the rest of the data frame
         for i in range(groupdata.shape[0]):
-            for j in range(groupdata.shape[-1]-1):
+            for j in range(groupdata.shape[-1]-2):
                 table_dict.cell(i,j).text = str(groupdata.values[i,j])
         
         for row in table_dict.rows:
             row.height = Cm(height)
 
-document.save('./'+bookname+'/'+bookname+'_Dictionary.docx') # Save document
+
+
+try:
+    document.save('./'+bookname+'/'+bookname+'_Dictionary.docx') # Save document
+    print('[{}] Document is created!!'.format(datetime.now().time()))
+except:
+    print('Word file is opened. Please close and re-run the program')
+    sys.exit()
+
+print('[{}] Saving new DB...'.format(datetime.now().time()))
+save_data.to_csv('./'+bookname+'/'+bookname+'_DB.csv',index=False, encoding='utf-8-sig')
+
+
+# In[9]:
+
+
+nondf = pd.DataFrame()
+
+
+# In[10]:
+
+
+print('[{}] Clearing {}_new.xlsx file...'.format(datetime.now().time(), bookname))
+nondf.to_excel('./' + bookname + '/' + bookname + '_new.xlsx', index=False)
 
 
 # In[ ]:
